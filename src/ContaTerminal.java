9a5a3c124310e413de287fe6317fc62f46739635
danilobarreto <danilobarreto15@gmail.com>
2024-04-30 13:58:30 -0300

    Desafio Java Basico

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,6 +11,17 @@ public class ContaTerminal {
         String nomeCliente;
         double saldo;
 
+        System.out.println("Por favor, digite o número da Agência !");
+        numero = sc.nextInt();
+        System.out.println("Digite sua Agência !");
+        agencia = sc.next();
+        System.out.println("Digite o nome do Cliente !");
+        nomeCliente = sc.next();
+        System.out.println("Digite o saldo do Cliente !");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo" + saldo +" já está disponível para saque");
+
 
     }
 }
