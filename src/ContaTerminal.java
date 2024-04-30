0cc60481643bcaa2f967609ceff0a78c6a2239e0
danilobarreto <danilobarreto15@gmail.com>
2024-04-30 13:46:51 -0300

    Desafio Java Basico

---
+++ src/ContaTerminal.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+
+    }
+}
